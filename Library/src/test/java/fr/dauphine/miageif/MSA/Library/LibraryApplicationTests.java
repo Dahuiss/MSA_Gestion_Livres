8bce8b73305372a036e74b5b4c5c095e2f43fa35
yufei-liu-source <75695069+yufei-liu-source@users.noreply.github.com>
2023-03-14 17:00:14 +0100

    initialisation of project

---
+++ Library/src/test/java/fr/dauphine/miageif/MSA/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dauphine.miageif.MSA.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
