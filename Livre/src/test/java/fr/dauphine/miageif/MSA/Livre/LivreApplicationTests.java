6004dc98f7b32a668324b40b8383cddff91a7557
yufei-liu-source <liuyufei9931@gmail.com>
2023-03-21 12:43:50 +0100

    Initialize books service

---
+++ Livre/src/test/java/fr/dauphine/miageif/MSA/Livre/LivreApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dauphine.miageif.MSA.Livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
